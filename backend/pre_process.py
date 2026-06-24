e53e086ec39fda31d06e870b7ec4c240a600d4c7
mrinmay2004 <mrinmay21012204@gmail.com>
2026-06-24 20:08:21 +0530

    update

--- backend/pre_process.py
+++ backend/pre_process.py
@@ -1,25 +1,86 @@
+# import string
+# from nltk.corpus import stopwords
+# import nltk
+# from nltk.stem.porter import PorterStemmer
+
+# ps = PorterStemmer()
+
+
+# def transform_text(text):
+#     text = text.lower()
+#     text = nltk.word_tokenize(text)
+#     y = []
+#     for i in text:
+#         if i.isalnum():
+#             y.append(i)
+#     text = y[:]
+#     y.clear()
+#     for i in text:
+#         if i not in stopwords.words('english') and i not in string.punctuation:
+#             y.append(i)
+#     text = y[:]
+#     y.clear()
+#     for i in text:
+#         y.append(ps.stem(i))
+#     return " ".join(y)
+
+
 import string
-from nltk.corpus import stopwords
 import nltk
+
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
+
+# ===============================
+# FIX FOR CLOUD DEPLOYMENT
+# ===============================
+
+nltk.download("punkt")
+nltk.download("punkt_tab")
+nltk.download("stopwords")
+
 
 ps = PorterStemmer()
 
 
 def transform_text(text):
+
+    # lowercase
     text = text.lower()
+
+
+    # tokenize
     text = nltk.word_tokenize(text)
+
+
+    # remove special characters
     y = []
+
     for i in text:
         if i.isalnum():
             y.append(i)
+
+
     text = y[:]
     y.clear()
+
+
+    # remove stopwords and punctuation
+    stop_words = set(stopwords.words("english"))
+
     for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
+        if i not in stop_words and i not in string.punctuation:
             y.append(i)
+
+
     text = y[:]
     y.clear()
+
+
+    # stemming
     for i in text:
         y.append(ps.stem(i))
+
+
     return " ".join(y)
